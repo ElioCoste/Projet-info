2a4d7e629a29b466e578cbfbd04e62fd4c4493f7
ElioCoste <elio.coste21@gmail.com>
2020-12-20 17:33:18 +0100

    Update projet.py

--- projet.py
+++ projet.py
@@ -23,17 +23,17 @@ def convertit_coup(coup):
 
 def coup_valide(plateau, coup):
     """Retourne True si le coup demandé est valide, False sinon."""
-    # Vérifie que la case de départ est différente de la case d'arrivée
-    if coup[0] == coup[1]:
-        return False
-
     # Vérifie que le coup proposé respecte le format convenu
     pattern = re.compile("[a-h][1-8][a-h][1-8]")
-    if re.fullmatch(coup.lower(), pattern) is None:
+    if re.fullmatch(pattern, coup.lower()) is None:
         return False
 
     # On peut donc convertir sans souci
     coup = convertit_coup(coup)
+
+    # Vérifie que la case de départ est différente de la case d'arrivée
+    if coup[0] == coup[1]:
+        return False
 
     # Vérifie que la case d'arrivée est atteignable par la pièce
     return coup[1] in DEPLACEMENTS[plateau[coup[0][0]][coup[0][1]]](plateau, coup[0])
@@ -55,35 +55,80 @@ def afficher_plateau(plateau, trait):
     # Affichage de l'échiquier
     for i in plateau:
         for j in i:
-            print(PIECES[j], end='')
+            print(PIECES[j], end=' ')
         print()
 
 
 ## Fonctions pour les déplacements possibles des pièces. 
 
 def pion(plateau, pos):
-    """Renvoie la liste des déplacements possibles pour un pion."""
-    return deplacements
+    """Donne la liste des coups valides pour un pion donné."""
+    """deplacements = []
+    colonne, ligne = pos
+    piece = plateau[ligne][colonne]
+
+    if piece.isupper(): # Regarde dans quel camp est le pion
+        ligne_devant_pion = plateau[pos[1] + 1]
+        Deux_ligne_devant_pion = plateau[pos[1] + 2]
+
+        if plateau[ligne-1][colonne] == '.': # Vérifie si le pion peut avancer d'une case
+            deplacements.append((ligne-1, colonne))
+            if Deux_ligne_devant_pion[colonne] == '.' and pos[1] == 1: #de deux cases
+                deplacements.append((colonne, pos[1] + 2))
+
+        if Ligne_devant_pion[colonne - 1].isupper(): #vérifie si le pion peut prendre en diago
+            deplacements.append((colonne - 1, pos[1] + 1))
+        if Ligne_devant_pion[colonne + 1].isupper():
+            deplacements.append((colonne + 1, pos[1] + 1))
+
+        if Ligne_devant_pion[colonne - 1] == '.' and ligne[colonne - 1] == 'p': # Vérifie si le pion peut prendre en passant(incomplet et faux)
+            deplacements.append((colonne - 1, pos[1] + 1))
+        if Ligne_devant_pion[colonne + 1] == '.' and ligne[colonne - 1] == 'p':
+            deplacements.append((colonne + 1, pos[1] + 1))
+
+    else: #pour l'autre camp
+        amis = ['r', 'n', 'b', 'q', 'k', 'p']
+        autre = 'P'
+
+        Ligne_devant_pion = plateau[pos[1] - 1]
+        Deux_ligne_devant_pion = plateau[pos[1] - 2]
+
+        if ligne != 0:
+
+            if Ligne_devant_pion[colonne] == '.':
+                deplacements.append((colonne, pos[1] - 1))
+                if Deux_ligne_devant_pion[colonne] == '.' and pos[1] == 6:
+                    deplacements.append((colonne, pos[1] - 2))
+
+            if Ligne_devant_pion[colonne - 1] not in amis and Ligne_devant_pion[colonne - 1] != '.':
+                deplacements.append((colonne - 1, pos[1] - 1))
+            if Ligne_devant_pion[colonne + 1] not in amis and Ligne_devant_pion[colonne + 1] != '.':
+                deplacements.append((colonne + 1, pos[1] - 1))
+
+            if Ligne_devant_pion[colonne - 1] == '.' and ligne[colonne - 1] == autre:
+                deplacements.append((colonne - 1, pos[1] - 1))
+            if Ligne_devant_pion[colonne + 1] == '.' and ligne[colonne + 1] == autre:
+                deplacements.append((colonne + 1, pos[1] - 1))
+
+    return deplacements"""
+    return [(4, 4)]
 
 def tour(plateau, pos):
     """Renvoie la liste des déplacements possibles pour une tour."""
     deplacements = []
     ligne, colonne = pos
-    liste_colonne = [0, 1, 2, 3, 4, 5, 6, 7]
-    liste_ligne = [0, 1, 2, 3, 4, 5, 6, 7]
-    for i in range(len(liste_ligne)):
-        for j in range(len(liste_colonne)):
-            if liste_ligne[i] == ligne:
-                deplacements += [(ligne, j)]
-    for j in range(len(liste_colonne)):
-        for i in range(len(liste_ligne)):
-            if liste_colonne[j] == colonne:
-                deplacements += [(i, colonne)]
+
+    # Cases sur la même ligne que la tour
+    for i in range(8):
+        deplacements.append((ligne, i))
+    # Cases sur la même colonne que la tour
+    for i in range(8):
+        deplacements.append((i, colonne))
+
     return deplacements
 
 def cavalier(plateau, pos):
     """Renvoie la liste des déplacements possibles pour un cavalier."""
-
     return deplacements
 
 def fou(plateau, pos):
@@ -111,7 +156,8 @@ PIECES = {
     "R": '♜',
     "B": '♝',
     "N": '♞',
-    "P": '♟'
+    "P": '♟',
+    ".": " "
 }
 
 DEPLACEMENTS = {
@@ -127,7 +173,7 @@ DEPLACEMENTS = {
 def demo():
     input("Appuyez sur ENTREE pour commencer une nouvelle partie.")
 
-    # Position de départ.
+    # pos de départ.
     plateau = [
         ['R', 'N', 'B', 'Q', 'K', 'B', 'K', 'R'], 
         ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'], 
@@ -139,14 +185,16 @@ def demo():
         ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']
     ]
 
-    trait = 0
+    trait = 1
     running = True
     while running:
         trait = 0 if trait == 1 else 1
         couleur = "Noirs" if trait else "Blancs"
 
-        coup = ((0, 0), (0, 0))
+        coup = ""
         while not coup_valide(plateau, coup):
-            coup = convertit_coup(input("Trait aux {}".format(couleur)))
+            coup = input("Trait aux {} : ".format(couleur))
 
+        coup = convertit_coup(coup)
         plateau = jouer_coup(plateau, coup)
+        afficher_plateau(plateau, trait)
