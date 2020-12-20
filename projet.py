a8fd3afc99d8ae70b56bfc16d978bf3568f48102
ElioCoste <elio.coste21@gmail.com>
2020-12-20 22:36:43 +0100

    pion

--- projet.py
+++ projet.py
@@ -19,7 +19,7 @@ def convertit_coup(coup):
     a, c = ord(a) - 97, ord(c) - 97
     return ((b, a), (d, c))
 
-def coup_valide(plateau, coup):
+def coup_valide(plateau, coup, trait):
     """Retourne True si le coup demandé est valide, False sinon.
     Incomplet."""
     # Vérifie que le coup proposé respecte le format convenu
@@ -30,18 +30,26 @@ def coup_valide(plateau, coup):
 
     # On peut donc convertir sans souci
     coup = convertit_coup(coup)
-
+    piece = plateau[coup[0][0]][coup[0][1]]
     # Vérifie que la case de départ est différente de la case d'arrivée
     if coup[0] == coup[1]:
         return False
 
+    # Vérifie que la case indiquée comporte bien une pièce de la bonne couleur
+    if piece.isupper() != trait or piece == ".":
+        return False
+
     # Vérifie que la case d'arrivée ne soit pas une pièce de même couleur que la pièce qui bouge
-    if (plateau[coup[0][0]][coup[0][1]].isupper() and plateau[coup[1][0]][coup[1][1]].isupper()) or \
-    (plateau[coup[0][0]][coup[0][1]].islower() and plateau[coup[1][0]][coup[1][1]].islower()):
+    if (piece.isupper() and plateau[coup[1][0]][coup[1][1]].isupper()) or \
+    (piece.islower() and plateau[coup[1][0]][coup[1][1]].islower()):
         return False
 
     # Vérifie que la case d'arrivée est atteignable par la pièce
-    return coup[1] in DEPLACEMENTS[plateau[coup[0][0]][coup[0][1]]](plateau, coup[0])
+    return coup[1] in DEPLACEMENTS[piece.lower()](plateau, coup[0])
+    
+    # Vérifie que le roi c'est pas en échec
+    #return not echec(jouer_coup(plateau, coup), trait)
+
 
 def jouer_coup(plateau, coup):
     """Joue le coup demandé sur l'échiquier."""
@@ -62,60 +70,60 @@ def afficher_plateau(plateau, trait):
             print(PIECES[j], end=' ')
         print()
 
+def echec(plateau, trait):
+    """Renvoie True si le roi qui a le trait est en échec, False sinon."""
+    return False
+
 
 ## Fonctions pour les déplacements possibles des pièces. 
 
 def pion(plateau, pos):
     """Donne la liste des coups valides pour un pion donné."""
-    """deplacements = []
-    colonne, ligne = pos
+    deplacements = []
+    ligne, colonne = pos
     piece = plateau[ligne][colonne]
 
-    if piece.isupper(): # Regarde dans quel camp est le pion
-        ligne_devant_pion = plateau[pos[1] + 1]
-        Deux_ligne_devant_pion = plateau[pos[1] + 2]
+    # Cas d'un pion noir
+    if piece.isupper():
+        if plateau[ligne+1][colonne] == ".":
+            deplacements.append((ligne+1, colonne))
+            # Si on est sur la ligne 1, on peut avancer de deux
+            if ligne == 1 and plateau[ligne+2][colonne] == '.':
+                deplacements.append((ligne+2, colonne))
+        
+        # Prise si il y a une pièce adverse
+        prise_droite, prise_gauche = True, True
+        if colonne == 0:
+            prise_gauche = False
+        elif colonne == 7:
+            prise_droite = False
+        
+        if prise_droite and plateau[ligne+1][colonne+1].islower():
+            deplacements.append((ligne+1, colonne+1))
+        if prise_gauche and plateau[ligne+1][colonne-1].islower():
+            deplacements.append((ligne+1, colonne-1))
 
-        if plateau[ligne-1][colonne] == '.': # Vérifie si le pion peut avancer d'une case
+    # Cas d'un pion blanc
+    else:
+        if plateau[ligne-1][colonne] == ".":
             deplacements.append((ligne-1, colonne))
-            if Deux_ligne_devant_pion[colonne] == '.' and pos[1] == 1: #de deux cases
-                deplacements.append((colonne, pos[1] + 2))
+            # Si on est sur la ligne 6, on peut avancer de deux
+            if ligne == 6 and plateau[ligne-2][colonne] == '.':
+                deplacements.append((ligne-2, colonne))
+        
+        # Prise si il y a une pièce adverse
+        prise_droite, prise_gauche = True, True
+        if colonne == 0:
+            prise_gauche = False
+        elif colonne == 7:
+            prise_droite = False
+        
+        if prise_droite and plateau[ligne-1][colonne+1].isupper():
+            deplacements.append((ligne-1, colonne+1))
+        if prise_gauche and plateau[ligne-1][colonne-1].isupper():
+            deplacements.append((ligne-1, colonne-1))
 
-        if Ligne_devant_pion[colonne - 1].isupper(): #vérifie si le pion peut prendre en diago
-            deplacements.append((colonne - 1, pos[1] + 1))
-        if Ligne_devant_pion[colonne + 1].isupper():
-            deplacements.append((colonne + 1, pos[1] + 1))
-
-        if Ligne_devant_pion[colonne - 1] == '.' and ligne[colonne - 1] == 'p': # Vérifie si le pion peut prendre en passant(incomplet et faux)
-            deplacements.append((colonne - 1, pos[1] + 1))
-        if Ligne_devant_pion[colonne + 1] == '.' and ligne[colonne - 1] == 'p':
-            deplacements.append((colonne + 1, pos[1] + 1))
-
-    else: #pour l'autre camp
-        amis = ['r', 'n', 'b', 'q', 'k', 'p']
-        autre = 'P'
-
-        Ligne_devant_pion = plateau[pos[1] - 1]
-        Deux_ligne_devant_pion = plateau[pos[1] - 2]
-
-        if ligne != 0:
-
-            if Ligne_devant_pion[colonne] == '.':
-                deplacements.append((colonne, pos[1] - 1))
-                if Deux_ligne_devant_pion[colonne] == '.' and pos[1] == 6:
-                    deplacements.append((colonne, pos[1] - 2))
-
-            if Ligne_devant_pion[colonne - 1] not in amis and Ligne_devant_pion[colonne - 1] != '.':
-                deplacements.append((colonne - 1, pos[1] - 1))
-            if Ligne_devant_pion[colonne + 1] not in amis and Ligne_devant_pion[colonne + 1] != '.':
-                deplacements.append((colonne + 1, pos[1] - 1))
-
-            if Ligne_devant_pion[colonne - 1] == '.' and ligne[colonne - 1] == autre:
-                deplacements.append((colonne - 1, pos[1] - 1))
-            if Ligne_devant_pion[colonne + 1] == '.' and ligne[colonne + 1] == autre:
-                deplacements.append((colonne + 1, pos[1] - 1))
-
-    return deplacements"""
-    return [(4, 4)]
+    return deplacements
 
 def tour(plateau, pos):
     """Renvoie la liste des déplacements possibles pour une tour."""
@@ -203,6 +211,7 @@ DEPLACEMENTS = {
 
 def demo():
     input("Appuyez sur ENTREE pour commencer une nouvelle partie.")
+    print()
 
     # pos de départ.
     plateau = [
@@ -219,13 +228,29 @@ def demo():
     trait = 1
     running = True
     while running:
+        afficher_plateau(plateau, trait)
         trait = 0 if trait == 1 else 1
         couleur = "Noirs" if trait else "Blancs"
 
         coup = ""
-        while not coup_valide(plateau, coup):
-            coup = input("Trait aux {} : ".format(couleur)).lower()
+        while not coup_valide(plateau[:], coup, trait):
+            coup = input("Trait aux {}. Entrez votre coup : ".format(couleur)).lower()
 
         coup = convertit_coup(coup)
-        plateau = jouer_coup(plateau, coup)
-        afficher_plateau(plateau, trait)
+
+        # piece_promue
+        piece_promue = None
+        if (plateau[coup[0][0]][coup[0][1]] == 'p' and coup[1][0] == 7)\
+            or (plateau[coup[0][0]][coup[0][1]] == 'P' and coup[1][0] == 0):
+            while piece_promue not in ('r', 'n', 'b', 'q'):
+                piece_promue = input("En quelle pièce voulez-vous promouvoir votre pion ? : ").lower()
+
+        plateau = jouer_coup(plateau[::], coup)
+
+        if piece_promue is not None:
+            print('Promotion')
+            if trait:
+                plateau[coup[1][0]][coup[1][1]] = piece_promue.upper()
+            else:
+                plateau[coup[1][0]][coup[1][1]] = piece_promue.lower()
+
